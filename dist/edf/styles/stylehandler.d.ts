0e6307d713bb91c5ec6bc2a5621722ce69a04b1a
Joel Ek <contact@joelek.se>
2023-12-19 16:29:08 +0100

    Generate dist artifacts.

--- dist/edf/styles/stylehandler.d.ts
+++ dist/edf/styles/stylehandler.d.ts
@@ -5,10 +5,16 @@ export declare class MissingTemplateError extends Error {
     constructor(template: string, type: string);
     toString(): string;
 }
+export declare class RecursiveTemplateError extends Error {
+    protected template: string;
+    protected type: string;
+    constructor(template: string, type: string);
+    toString(): string;
+}
 type Style<A extends keyof format.Templates> = Exclude<Required<format.Templates>[A][string], undefined>;
 export declare class StyleHandler {
     protected templates: format.Templates;
-    protected getStyle<A extends keyof format.Templates>(type: A, style: Style<A> | undefined): Style<A> | undefined;
+    protected getStyle<A extends keyof format.Templates>(type: A, style: Style<A> | undefined, exclude: Array<string>): Style<A> | undefined;
     constructor(templates: format.Templates | undefined);
     getBoxStyle(style?: format.BoxNodeStyle): format.BoxNodeStyle | undefined;
     getHorizontalStyle(style?: format.BoxNodeStyle): format.BoxNodeStyle | undefined;
--- dist/edf/styles/stylehandler.js
+++ dist/edf/styles/stylehandler.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.StyleHandler = exports.MissingTemplateError = void 0;
+exports.StyleHandler = exports.RecursiveTemplateError = exports.MissingTemplateError = void 0;
 class MissingTemplateError extends Error {
     template;
     type;
@@ -10,14 +10,28 @@ class MissingTemplateError extends Error {
         this.type = type;
     }
     toString() {
-        return `Expected template "${this.template}" to be a exist for type "${this.type}"!`;
+        return `Expected template "${this.template}" for type "${this.type}" to exist!`;
     }
 }
 exports.MissingTemplateError = MissingTemplateError;
 ;
+class RecursiveTemplateError extends Error {
+    template;
+    type;
+    constructor(template, type) {
+        super();
+        this.template = template;
+        this.type = type;
+    }
+    toString() {
+        return `Expected template "${this.template}" for type "${this.type}" to not be used recursively!`;
+    }
+}
+exports.RecursiveTemplateError = RecursiveTemplateError;
+;
 class StyleHandler {
     templates;
-    getStyle(type, style) {
+    getStyle(type, style, exclude) {
         if (style == null) {
             return;
         }
@@ -25,13 +39,16 @@ class StyleHandler {
         if (template == null) {
             return style;
         }
+        if (exclude.includes(template)) {
+            throw new RecursiveTemplateError(template, type);
+        }
         let templates = (this.templates[type] ?? {});
         let template_style = templates[template];
         if (template_style == null) {
             throw new MissingTemplateError(template, type);
         }
         return {
-            ...this.getStyle(type, template_style),
+            ...this.getStyle(type, template_style, [...exclude, template]),
             ...style
         };
     }
@@ -39,16 +56,16 @@ class StyleHandler {
         this.templates = templates ?? {};
     }
     getBoxStyle(style) {
-        return this.getStyle("box", style);
+        return this.getStyle("box", style, []);
     }
     getHorizontalStyle(style) {
-        return this.getStyle("horizontal", style);
+        return this.getStyle("horizontal", style, []);
     }
     getTextStyle(style) {
-        return this.getStyle("text", style);
+        return this.getStyle("text", style, []);
     }
     getVerticalStyle(style) {
-        return this.getStyle("vertical", style);
+        return this.getStyle("vertical", style, []);
     }
 }
 exports.StyleHandler = StyleHandler;
