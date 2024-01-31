c1a5101e163502e948c5a8c459aca5b89ced78fb
Joel Ek <contact@joelek.se>
2024-01-31 15:37:37 +0100

    Add methods for decoding PNG image data.

--- source/png/parser.ts
+++ source/png/parser.ts
@@ -1,5 +1,5 @@
 import { Chunk } from "@joelek/ts-stdlib/dist/lib/data/chunk";
-import { BitstreamReader, BitstreamWriter } from "../shared";
+import { inflate } from "../shared";
 
 export enum ColorType {
 	GRAYSCALE = 0,
@@ -29,6 +29,10 @@ export function parseIHDRChunk(buffer: ArrayBuffer) {
 	let height = dw.getUint32(offset); offset += 4;
 	let bit_depth = dw.getUint8(offset); offset += 1;
 	let color_type = ColorType[dw.getUint8(offset)]; offset += 1;
+	let permitted_bit_depths = getPermittedBitDepths(color_type);
+	if (!permitted_bit_depths.includes(bit_depth)) {
+		throw new Error(`Expected a permitted bit depth of ${permitted_bit_depths.join(", ")} for color type ${color_type}!`);
+	}
 	let compression_method = CompressionMethod[dw.getUint8(offset)]; offset += 1;
 	let filter_method = FilterMethod[dw.getUint8(offset)]; offset += 1;
 	let interlace_method = InterlaceMethod[dw.getUint8(offset)]; offset += 1;
@@ -41,6 +45,50 @@ export function parseIHDRChunk(buffer: ArrayBuffer) {
 		filter_method,
 		interlace_method
 	};
+};
+
+export type IHDRChunk = ReturnType<typeof parseIHDRChunk>;
+
+export function getPermittedBitDepths(color_type: typeof ColorType[number]): Array<number> {
+	if (color_type === "GRAYSCALE") {
+		return [1, 2, 4, 8, 16];
+	}
+	if (color_type === "TRUECOLOR") {
+		return [8, 16];
+	}
+	if (color_type === "INDEXED") {
+		return [1, 2, 4, 8];
+	}
+	if (color_type === "GRAYSCALE_AND_ALPHA") {
+		return [8, 16];
+	}
+	if (color_type === "TRUECOLOR_AND_ALPHA") {
+		return [8, 16];
+	}
+	throw new Error(`Expected a valid color type!`);
+};
+
+export function getNumberOfChannels(color_type: typeof ColorType[number]): number {
+	if (color_type === "GRAYSCALE") {
+		return 1;
+	}
+	if (color_type === "TRUECOLOR") {
+		return 3;
+	}
+	if (color_type === "INDEXED") {
+		return 1;
+	}
+	if (color_type === "GRAYSCALE_AND_ALPHA") {
+		return 2;
+	}
+	if (color_type === "TRUECOLOR_AND_ALPHA") {
+		return 4;
+	}
+	throw new Error(`Expected a valid color type!`);
+};
+
+export function getBitsPerPixel(ihdr: IHDRChunk): number {
+	return getNumberOfChannels(ihdr.color_type) * ihdr.bit_depth;
 };
 
 export function parsePNGChunk(buffer: ArrayBuffer, offset: number) {
@@ -86,3 +134,87 @@ export function parsePNGData(buffer: ArrayBuffer) {
 };
 
 export type PNGData = ReturnType<typeof parsePNGData>;
+
+export function averagePredictor(left: number, top: number): number {
+	return (left + top) >> 2;
+};
+
+export function paethPredictor(left: number, top: number, top_left: number): number {
+	let p = left + top - top_left;
+	let pa = Math.abs(p - left);
+	let pb = Math.abs(p - top);
+	let pc = Math.abs(p - top_left);
+	if (pa <= pb && pa <= pc) {
+		return left;
+	} else if (pb <= pc) {
+		return top;
+	} else {
+		return top_left;
+	}
+};
+
+export function modulo(number: number, modulo: number): number {
+	return ((number % modulo) + modulo) % modulo;
+};
+
+export function decodeImageData(png: PNGData): Uint8Array {
+	let idats = png.chunks.filter((chunk) => chunk.type === "IDAT");
+	if (idats.length !== 1) {
+		throw new Error(`Expected exactly one IDAT chunk!`);
+	}
+	let deflated_idat = idats[0].data;
+	let inflated_idat = inflate(deflated_idat);
+	let bits_per_pixel = getBitsPerPixel(png.ihdr);
+	let x_delta = Math.ceil(bits_per_pixel / 8);
+	let y_delta = 1;
+	let bytes_per_scanline = Math.ceil(bits_per_pixel * png.ihdr.width / 8);
+	let bytes = [] as Array<number>;
+	let offset = 0;
+	function getLeftByte(x: number, y: number): number {
+		return x >= x_delta ? bytes[(y) * bytes_per_scanline + (x - x_delta)] : 0;
+	}
+	function getTopByte(x: number, y: number): number {
+		return y >= y_delta ? bytes[(y - y_delta) * bytes_per_scanline + (x)] : 0;
+	}
+	function getTopLeftByte(x: number, y: number): number {
+		return y >= y_delta && x >= x_delta ? bytes[(y - y_delta) * bytes_per_scanline + (x - x_delta)] : 0;
+	}
+	for (let y = 0; y < png.ihdr.height; y++) {
+		let predictor = inflated_idat[offset++];
+		if (predictor === 0) {
+			for (let x = 0; x < bytes_per_scanline; x++) {
+				bytes.push(inflated_idat[offset++]);
+			}
+		} else if (predictor === 1) {
+			for (let x = 0; x < bytes_per_scanline; x++) {
+				let left_byte = getLeftByte(x, y);
+				let byte = modulo(inflated_idat[offset++] + left_byte, 256);
+				bytes.push(byte);
+			}
+		} else if (predictor === 2) {
+			for (let x = 0; x < bytes_per_scanline; x++) {
+				let top_byte = getTopByte(x, y);
+				let byte = modulo(inflated_idat[offset++] + top_byte, 256);
+				bytes.push(byte);
+			}
+		} else if (predictor === 3) {
+			for (let x = 0; x < bytes_per_scanline; x++) {
+				let left_byte = getLeftByte(x, y);
+				let top_byte = getTopByte(x, y);
+				let byte = modulo(inflated_idat[offset++] + averagePredictor(left_byte, top_byte), 256);
+				bytes.push(byte);
+			}
+		} else if (predictor === 4) {
+			for (let x = 0; x < bytes_per_scanline; x++) {
+				let left_byte = getLeftByte(x, y);
+				let top_byte = getTopByte(x, y);
+				let top_left_byte = getTopLeftByte(x, y);
+				let byte = modulo(inflated_idat[offset++] + paethPredictor(left_byte, top_byte, top_left_byte), 256);
+				bytes.push(byte);
+			}
+		} else {
+			throw new Error(`Expected a valid PNG predictor!`);
+		}
+	}
+	return Uint8Array.from(bytes);
+};
